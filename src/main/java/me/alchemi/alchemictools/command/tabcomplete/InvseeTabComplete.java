67012525c78bebff9321069ddf56f1e8865bf341
Alchemi <misha.smit@xs4all.nl>
2019-10-10 08:33:37 +0200

    Fixed invsee permission error

--- src/main/java/me/alchemi/alchemictools/command/tabcomplete/InvseeTabComplete.java
+++ src/main/java/me/alchemi/alchemictools/command/tabcomplete/InvseeTabComplete.java
@@ -11,6 +11,7 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 import me.alchemi.al.objects.base.TabCompleteBase;
+import me.alchemi.alchemictools.objects.Permissions;
 
 public class InvseeTabComplete extends TabCompleteBase{
 
@@ -19,14 +20,16 @@ public class InvseeTabComplete extends TabCompleteBase{
 
 		List<Object> list = new ArrayList<Object>();
 
-		if (!(sender instanceof Player && !sender.hasPermission(cmd.getPermission())))
+		if (!(sender instanceof Player 
+				|| Permissions.INVSEE.check(sender)))
 			return Arrays.asList("");
 
 		if (args.length == 1) {
 			list.addAll(Bukkit.getOnlinePlayers().stream()
 					.map(player -> player.getDisplayName())
 					.collect(Collectors.toSet()));
-		} else if (args.length == 2) {
+			
+		} else if (args.length > 1) {
 			list.add("inventory");
 			list.add("potions");
 			list.add("armour");
